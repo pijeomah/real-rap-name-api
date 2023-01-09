6ca021c507d764d5a0c7066d2787839081b40d83
pijeomah <promiseij4919@gmail.com>
2023-01-09 14:52:07 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+const artiste = {
+    "lil wayne":{
+        "birthName": "Dwayne Michael Carter",
+        "age": 40,
+        "birthPlace" : "New Orleans, Louisana"
+    },
+    "birdman":{
+        "birthName": "Bryan Christopher Williams",
+        "age": 52,
+        "birthPlace": "New Orleans, Louisana"
+    },
+
+    "drake":{
+        "birthName": "Aubrey Drake Graham",
+        "age": 36,
+        "birthPlace": "Ontario, Canada"
+    },
+
+    "nicki minaj":{
+        "birhName": "Onika Tanya Maraj",
+        "age": 40,
+        "birthPlace": "St.James, Trindad and Tobago"
+    },
+
+    "tyga":{
+        "birthName": "Michael Ray Nguyen-Stevenson",
+        "age": 33,
+        "birthPlace": "Compton,California"
+    },
+    "unknown":{
+        "birthName": "unknown",
+        "age": 0,
+        "birthPlace": "unknown"
+    }
+}
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req,res)=>{
+    const artistebName = req.params.name.toLowerCase()
+  if(artiste[artistebName]){
+    res.json(artiste[artistebName])
+  }else {
+    res.json('unknown')
+  }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log("Listening on 8080")
+})
